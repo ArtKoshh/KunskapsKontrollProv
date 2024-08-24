93a0b7965009e385b941f1e58c2496256f37cc22
ArtKoshh <artur.koshmin@hotmail.com>
2024-08-24 20:29:48 +0200

    Add project files.

---
+++ KunskapsKontrollProv/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Task-1 
+Add two new variables to your program that stores string values. One of them is going 
+to store your first name and the other your last name, so give them informative 
+names. Then let the program out put the following text on console.
+“Hello <firstname> <lastname>! I’m glad to inform you that you are the test 
+subject of my very first assignment!”*/
+
+
+using System.Net.Security;
+using System.Runtime.InteropServices;
+
+//string Name = "";
+//string lastname = "";
+Console.WriteLine("Name?");string Name = Console.ReadLine();Console.WriteLine("Lastname?");string lastname = Console.ReadLine();Console.WriteLine($" Hello {Name},{lastname}");
+Console.ReadKey();
+
+/*
+* Task-2 
+Create three newvariables oftype Date Time andletthemstoreyesterdays,todaysand
+tomorrowsdate. Rememberto give theminformative names. Thenlet yourprogram
+output them to the screen like:
+“Todays date is <todays date>” 
+“Tomorrows date is <tomorrows date>”
+“Yesterdays date was <yesterdays date>”
+*/
+
+DateTime _now = DateTime.Now;
+DateTime _tomorrow = _now.AddDays(1);
+DateTime _yesterday = _now.AddDays(-1);
+Console.WriteLine($"Today is {_now},\nTomorrow is{_tomorrow},\n Yesterday was{_yesterday}");
+Console.ReadKey();
